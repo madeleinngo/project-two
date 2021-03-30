61d5998086d0980c5cf81d0b8f96c344d9e77a87
Madelein Ngo <wumeijin@live.unc.edu>
2021-03-30 15:15:30 -0400

    implementing toggle navbar and adding more assets

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,38 @@
+var toggleButton = document.querySelector(".toggle");
+var links = document.querySelector(".links");
+
+function toggle() {
+  links.classList.toggle("active");
+}
+
+toggleButton.addEventListener("click", toggle);
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
-function currentSlide(n) {
-    showSlides (slideIndex = n);
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-function showSlides (n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var spans = document.getElementsByClassName("stories-bar")
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
